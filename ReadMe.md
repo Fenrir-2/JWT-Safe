c7a336aa4d773d6f3283fa0798cb66a2b990e539
Fenrir-2 <nicolas.fonnier@gmail.com>
2020-04-22 06:32:43 +0200

    Added doc, ReadMe, and logic skeleton

---
+++ ReadMe.md
@@ -0,0 +1,8 @@
+# JWT-Safe
+## How to build the project
+
+At the root of the project, just run 
+
+```bash
+docker-compose up --build
+```
--- api_apr/app.py
+++ api_apr/app.py
@@ -1,7 +1,26 @@
 from flask import Flask
+from flask import request
+import flasgger
+import json
 
-app = Flask(__name__)
+app = Flask("Protected Ressource Access")
+swagger = flasgger.Swagger(app)
 
-@app.route('/')
-def hello():
-    return 'Hm, you shouldn\'t be able to see that. Weird.'
+@app.route('/api/access', methods=['GET'])
+@flasgger.swag_from('docs/token_check.yml')
+def token_check():
+    token = request.args.get('token', default='', type=str)
+    if token == '':
+        return json.dumps({"Error" : "No token provided"})
+
+    context = zmq.Context()
+    try:
+        socket = context.socket(zmq.REQ)
+        socket.connect("tcp://token_dealer:7000")
+        socket.send_json({"check" : token})
+    except:
+        return json.dumps({"Error" : "No token dealer"})
+
+    if message['status'] != "ok":
+        return abort(403)
+        
